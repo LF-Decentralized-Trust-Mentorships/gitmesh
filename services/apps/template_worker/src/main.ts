86a20db4bf324de55f048c076e39a9704a3644e4
RAWx18 <rawx18.dev@gmail.com>
2025-12-15 16:30:36 +0530

    added workers template
    Signed-off-by: RAWx18 <rawx18.dev@gmail.com>

---
+++ services/apps/template_worker/src/main.ts
@@ -0,0 +1,28 @@
+import { Config } from '@gitmesh/archetype-standard'
+import { ServiceWorker, Options } from '@gitmesh/archetype-worker'
+
+const config: Config = {
+  envvars: [],
+  producer: {
+    enabled: false,
+  },
+  temporal: {
+    enabled: false,
+  },
+  redis: {
+    enabled: false,
+  },
+}
+
+const options: Options = {
+  postgres: {
+    enabled: false,
+  },
+}
+
+const svc = new ServiceWorker(config, options)
+
+setImmediate(async () => {
+  await svc.init()
+  await svc.start()
+})
